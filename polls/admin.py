f62f071ed3a41db91b9c2e3c1f831b5bb78ca2f8
zxcv5608 <asdfghjkl123620@gmail.com>
2020-07-23 10:02:33 +0800

    finish the final template page setting

--- polls/admin.py
+++ polls/admin.py
@@ -1,10 +1,23 @@
 from django.contrib import admin
 
 # Register your models here.
-from .models import Question
+from .models import Choice, Question
+
+#告诉 Django：“Choice 对象要在 Question 后台页面编辑。默认提供 4 个足够的选项字段。”
+#透過TabularInline代替StackedInline以一種表格式的展示方式顯得更加緊湊
+class ChoiceInline(admin.TabularInline):
+    model = Choice
+    extra = 4
 
 class QuestionAdmin(admin.ModelAdmin):
-    fields = ['pub_date', 'question_text']
+    fieldsets = [
+        (None,              {'fields':['question_text']}),
+        ('Date information',{'fields':['pub_date'],'classes': ['collapse']}),
+    ]
+    inlines = [ChoiceInline]
+    list_display = ('question_text','pub_date','was_published_recently')
+    list_filter = ['pub_date']
+    search_fields = ['question_text']
 
 
-admin.site.register(Question,QuestionAdmin)
+admin.site.register(Question, QuestionAdmin)
